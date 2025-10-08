a49057506a7fcb3072c3f5c1fa7c1823c8976f9d
Mohamed Saeed <74047939+EngMohamedSaeed2001@users.noreply.github.com>
2025-10-08 21:38:44 +0300

    v8-fix
    - remove default login credentials
    - edit logo tab

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("saeed@examble.com");
-  const [password, setPassword] = useState("Pass@123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLogin } = useLogin();
 
   function handleSubmit(e) {
