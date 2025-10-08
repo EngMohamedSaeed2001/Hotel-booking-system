dc6447cfd2a1e538d3dd7111bcd1cd6f5ce539ba
Mohamed Saeed <74047939+EngMohamedSaeed2001@users.noreply.github.com>
2025-10-08 03:53:18 +0300

    v7-dashboard
    -create Stats

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,10 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Stats from "./Stats";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +12,30 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+  const {
+    stays,
+    isLoading: isLoadingStays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+
+  const { cabins, isLoading: isLoadingCbins } = useCabins();
+
+  if (isLoadingBookings || isLoadingStays || isLoadingCbins) return <Spinner />;
+
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={bookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
